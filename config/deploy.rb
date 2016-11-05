e6eb187f37d67096d417df272972adabc96f89a7
Travis Spangle <travis.spangle@k2sports.com>
2016-11-04 23:31:34 -0700

    -mconfiguring capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.6.1'
 
 set :application, 'voteaid'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:TravisSpangle/vote-aid-api.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp

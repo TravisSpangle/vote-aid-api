3b5b33aab24c5553c96205fcb3049bcbe0732114
Travis Spangle <travis.spangle@k2sports.com>
2016-11-05 22:06:34 -0700

    save users answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,19 @@
 class AnswersController < ApplicationController
+  before_action :load_user
+
   def create
+    @answer             = answer
+    @answer.question_id = params[:question_id]
+    @answer.value       = params[:answer]
+
+    if @answer.save
+      render json: { answer: @answer.value, message: 'success' }
+    else
+      render status: 500
+    end
+  end
+
+  def answer
+    @user.answers.find_by_question_id(params[:question_id]) || @user.answers.new
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+
+  private
+
+  def load_user
+    @user = User.find_by_token(params[:token]) 
+    render status: 401 if @user.nil?
+  end
 end
